141e8b35af13f3cd1d958ff09c7ea0517751f9c0
dorp <dorp@cs.uni-bonn.de>
2019-01-20 18:17:27 +0100

    added real dashboard tests

--- src/test/unit/app/test_dashboard.py
+++ src/test/unit/app/test_dashboard.py
@@ -1,13 +1,34 @@
 import pytest
-
-from app.app import APP
-
-
-@pytest.fixture(scope='function')
-def mock_app():
-    return APP.test_client()
+from test.unit.common import MockUser
 
 
-def test_home_dashboard(mock_app):
+@pytest.fixture(scope='function', autouse=True)
+def patch_current_user(monkeypatch):
+    monkeypatch.setattr('app.dashboard.current_user', MockUser())
+
+
+def test_home_dashboard(mock_app, app_setup):
+    app_setup.mett_store.create_account(MockUser.email)
+
     response = mock_app.get('/')
-    assert response
+    assert 'Welcome {}!'.format(MockUser.email).encode() in response.data
+    assert b'There is no current order!' in response.data
+
+
+def test_dashboard_order_exists(mock_app, app_setup):
+    app_setup.mett_store.create_account(MockUser.email)
+    app_setup.mett_store.create_order()
+
+    response = mock_app.get('/')
+    assert 'Welcome {}!'.format(MockUser.email).encode() in response.data
+    assert not b'There is no current order!' in response.data
+
+
+def test_dashboard_order_history(mock_app, app_setup):
+    app_setup.mett_store.create_account(MockUser.email)
+    app_setup.mett_store.create_order()
+    app_setup.mett_store.order_bun(MockUser.email, 'Weizen')
+    app_setup.mett_store.expire_order()
+
+    response = mock_app.get('/')
+    assert b'You have ordered a mean of 1.0 buns' in response.data
