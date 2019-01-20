1e2f684cae6a4dcebc09ca917c7ea7e21ad75862
dorp <dorp@cs.uni-bonn.de>
2019-01-20 21:20:00 +0100

    add tests for password reset

--- src/test/unit/app/test_profile.py
+++ src/test/unit/app/test_profile.py
@@ -14,5 +14,20 @@ def test_show_profile_page(mock_app):
 
 
 def test_profile_new_password(mock_app):
-    response = mock_app.post('/profile', data={'new_password': 'any_password', 'new_password_confirm': 'any_password', 'old_password': 'old_password'}, follow_redirects=True)
-    assert response.status_code == 200
+    response = mock_app.post('/profile', data={'new_password': 'any_password', 'new_password_confirm': 'any_password', 'old_password': 'old_password'})
+    assert b'password change successful' in response.data
+
+
+def test_profile_wrong_password(mock_app):
+    response = mock_app.post('/profile', data={'new_password': 'any_password', 'new_password_confirm': 'any_password', 'old_password': 'false_password'})
+    assert b'wrong password' in response.data
+
+
+def test_profile_non_matching_password(mock_app):
+    response = mock_app.post('/profile', data={'new_password': 'any_password', 'new_password_confirm': 'other_password', 'old_password': 'old_password'})
+    assert b'new password did not match' in response.data
+
+
+def test_profile_illegal_password(mock_app):
+    response = mock_app.post('/profile', data={'new_password': '', 'new_password_confirm': '', 'old_password': 'old_password'})
+    assert b'password is not legal' in response.data
