aed8f96afb5b1a81d645e408e4b26b3aab9cb0c8
dorp <dorp@cs.uni-bonn.de>
2019-02-05 15:59:24 +0100

    experimental uwsgi mountpoint support

--- src/app/app_setup.py
+++ src/app/app_setup.py
@@ -5,12 +5,36 @@ from pathlib import Path
 from flask import Flask
 
 from app.admin import AdminRoutes
-from app.orders import OrderRoutes
 from app.dashboard import DashboardRoutes
+from app.orders import OrderRoutes
 from app.profile import ProfileRoutes
 from app.security.authentication import add_flask_security_to_app
 from app.user import UserRoutes
 from database.mett_store import MettStore
+
+
+class ReverseProxied(object):
+
+    def __init__(self, app, script_name=None, scheme=None, server=None):
+        self.app = app
+        self.script_name = script_name
+        self.scheme = scheme
+        self.server = server
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '') or self.script_name
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+        scheme = environ.get('HTTP_X_SCHEME', '') or self.scheme
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        server = environ.get('HTTP_X_FORWARDED_SERVER', '') or self.server
+        if server:
+            environ['HTTP_HOST'] = server
+        return self.app(environ, start_response)
 
 
 class AppSetup:
@@ -33,3 +57,5 @@ class AppSetup:
         AdminRoutes(self.app, self.config, self.mett_store)
         ProfileRoutes(self.app, self.config, self.user_database, self.user_interface)
         UserRoutes(self.app, self.config, self.mett_store, self.user_database, self.user_interface)
+
+        self.app = ReverseProxied(self.app, script_name='/other')
